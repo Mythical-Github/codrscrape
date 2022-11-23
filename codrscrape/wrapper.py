5a162b6bcc3a82d0652958c89ecab991e0653342
Mythical-Github <67753356+Mythical-Github@users.noreply.github.com>
2022-11-22 21:55:25 -0500

    More Refactoring
    more refactoring

--- codrscrape/wrapper.py
+++ codrscrape/wrapper.py
@@ -8,12 +8,12 @@ import requests
 import subprocess
 
 exception_array = []
-folder_to_process = r"D:\codrscrape\Output"
-seven_zip_7z_exe = r"C:\Program Files\7-Zip\7z.exe"
 
-link_list = {
+DIR_TO_PROCESS = pathlib.Path(r"..\Output")
+SEVEN_ZIP_7Z_EXE = r"C:\Program Files\7-Zip\7z.exe"
+LINK_LIST = {
     r"WaW Campaign Maps": r"https://callofdutyrepo.com/waw-campaign-maps/",
-    r"Waw Zombie Maps": r"https://callofdutyrepo.com/wawmaps/",
+    #    r"Waw Zombie Maps": r"https://callofdutyrepo.com/wawmaps/",
     r"WaW Mods": r"https://callofdutyrepo.com/waw-mods/",
     r"BO1 Mods": r"https://callofdutyrepo.com/bo1-mods/",
     r"BO1 Maps": r"https://callofdutyrepo.com/bo1-maps-by-name/",
@@ -36,32 +36,33 @@ class CoDSpecific:
         print("Wrapper Finished")
 
     @staticmethod
-    def download_info():
-        for link in link_list:
-            path_arg = folder_to_process + "\\" + link.replace(" ", "_")
-            archive_arg = f"{path_arg}\\Archive {link}.txt".replace(" ", "_")
-            if not os.path.isdir(path_arg):
-                os.makedirs(path_arg)
-            subprocess.run(
-                f"codrscrape.exe -l --path {path_arg} --archive {archive_arg} -w --to-screen {link_list.get(link)})")
+    def download_info(input_dir: pathlib.Path):
+        for link in LINK_LIST:
+            path = pathlib.Path(f"{str(input_dir)}\\link".replace(" ", "_"))
+            archive = pathlib.Path(f"{path}\\Archive {link}.txt".replace(" ", "_"))
+            if not pathlib.Path.is_dir(path):
+                pathlib.Path.mkdir(path)
+            subprocess.run(f"codrscrape.exe -l --path {path} --archive {archive} -w --to-screen {LINK_LIST.get(link)})")
 
     @staticmethod
     def download_mods(input_array):
-        for metadata_json in input_array:
-            if metadata_json.endswith("metadata.json"):
-                file_path = f"{os.path.split(metadata_json)[0]}\\mod.zip"
+        for json_ in input_array:
+            if json_.endswith("metadata.json"):
+                test = pathlib.Path(json_)
+                test_2 = pathlib.Path.stem(test)
+                file_path = pathlib.Path(f"{test_2}\\mod.zip")
                 print()
                 print(f"Checking if {file_path} Already Exists")
-                if os.path.isfile(file_path):
+                if pathlib.Path.is_file(file_path):
                     print(f"{file_path} Already Exists")
                     print(f"Checking {file_path} Validity")
                     if does_file_begin_with_str(file_path, "<!-- Copyright"):
                         if os.path.isfile(file_path):
                             os.remove(file_path)
                             print("File Wasn't Valid, Checking Download Link")
-                            if is_download_link_functional(get_item_from_json(metadata_json, "download")):
+                            if is_download_link_functional(get_item_from_json(json_, "download")):
                                 print("Download Link Functional, Re-downloading")
-                                download_file(get_item_from_json(metadata_json, "download"), file_path)
+                                download_file(get_item_from_json(json_, "download"), file_path)
                                 print("File Downloaded")
                                 if does_file_begin_with_str(file_path, "<!-- Copyright"):
                                     exception_array.append(file_path)
@@ -73,9 +74,9 @@ class CoDSpecific:
                 else:
                     print("Mod Archive Doesn't Currently Exist")
                     print("Checking Download Link Validity")
-                    if is_download_link_functional(get_item_from_json(metadata_json, "download")):
+                    if is_download_link_functional(get_item_from_json(json_, "download")):
                         print("Download Link Functional, Downloading")
-                        download_file(get_item_from_json(metadata_json, "download"), file_path)
+                        download_file(get_item_from_json(json_, "download"), file_path)
                         print("File Downloaded")
                         if not does_file_begin_with_str(file_path, "<!-- Copyright"):
                             exception_array.append(file_path)
@@ -84,32 +85,33 @@ class CoDSpecific:
                         exception_array.append(file_path)
 
     @staticmethod
-    def organize_unzipped_mods():
-        for root, dirs, files in os.walk(folder_to_process):
+    def organize_unzipped_mods(input_dir: pathlib.Path):
+        for root, dirs, files in os.walk(input_dir):
             if r"$1" in root:
                 if root.endswith(r"$1"):
                     if len(os.listdir(root)) == 0:
                         shutil.rmtree(root)
                     else:
+                        shutil.move(pathlib.Path(root), get_one_dir_up(root))
                         print()
             if r"$PLUGINSDIR" in root:
                 shutil.rmtree(root)
                 print(f"{root} Was Removed")
 
     @staticmethod
-    def extract_mod_archives():
-        for file in get_files_by_extension_in_tree(folder_to_process, ".zip"):
-            extract_archive(file, f"{file}_unzipped")
+    def extract_mod_archives(input_dir: pathlib.Path):
+        for file in get_files_by_extension_in_tree(input_dir, ".zip"):
+            extract_archive(file, pathlib.Path(f"{file}_unzipped"))
 
 
-def does_file_begin_with_str(input_file: str, input_string: str) -> bool:
+def does_file_begin_with_str(input_file: pathlib.Path, input_string: str) -> bool:
     # noinspection PyBroadException
     try:
         if open(input_file).read(len(input_string)) == input_string:
             pathlib.Path.unlink(pathlib.Path(input_file))
             return True
         else:
-            os.remove(input_file)
+            pathlib.Path.unlink(input_file)
             return True
     except:
         return False
@@ -124,34 +126,34 @@ def install_seven_zip():
     subprocess.run("winget install 7zip.7zip -h")
 
 
-def get_files_by_extension_in_tree(input_dir: str, input_type: str) -> list:
+def get_files_by_extension_in_tree(input_dir: pathlib.Path, input_type: str) -> list:
     file_array = []
     for root, dirs, files in os.walk(input_dir):
         for file in files:
             if file.endswith(input_type):
-                file_array.append(f"{root}\\{file}")
+                file_array.append(pathlib.Path(f"{root}\\{file}"))
     return file_array
 
 
-def extract_archive(input_archive: str, output_folder: str):
-    if not os.path.isfile(seven_zip_7z_exe):
+def extract_archive(input_archive: pathlib.Path, output_folder: pathlib.Path):
+    if not pathlib.Path(SEVEN_ZIP_7Z_EXE):
         install_seven_zip()
     try:
-        if os.path.isfile(input_archive):
-            if not os.path.isdir(output_folder):
-                os.makedirs(output_folder)
-                subprocess.run(f"{seven_zip_7z_exe} x -y {input_archive} -o{output_folder}")
+        if pathlib.Path.is_file(input_archive):
+            if not pathlib.Path.is_dir(output_folder):
+                pathlib.Path.mkdir(output_folder)
+                subprocess.run(f"{SEVEN_ZIP_7Z_EXE} x -y {input_archive} -o{output_folder}")
                 print(f"{input_archive} has extracted successfully")
             else:
                 print(f"{input_archive} has already been extracted successfully")
         return True
-    except Exception as ex:
-        print(ex)
+    except Exception as FailedExtractionError:
+        print(FailedExtractionError)
         print(f"{input_archive} failed to extract")
 
 
-def del_files_by_ext_in_tree(input_dir: str, file_type: str):
-    for root, dirs, files in os.walk(input_dir):
+def del_files_by_ext_in_tree(input_dir: pathlib.Path, file_type: str):
+    for root, dirs, files in os.walk(str(input_dir)):
         for file in files:
             if file.endswith(file_type):
                 pathlib.Path.unlink(pathlib.Path(f"{root}\\{file}"))
@@ -168,13 +170,13 @@ def is_download_link_functional(download_link: str) -> bool:
         return False
 
 
-def download_file(input_link: str, output_path: str):
+def download_file(input_link: str, output_path: pathlib.Path):
     if input_link:
-        open(output_path, 'wb').write(requests.get(input_link, allow_redirects=True).content)
+        open(output_path, 'wb').write(requests.get(str(input_link), allow_redirects=True).content)
 
 
-def get_item_from_json(input_json: str, input_key: str) -> str:
-    if os.path.isfile(input_json):
+def get_item_from_json(input_json: pathlib.Path, input_key: str) -> str:
+    if pathlib.Path.is_file(input_json):
         with open(input_json, "r") as output_item:
             item = json.load(output_item)[input_key]
         return item
@@ -183,25 +185,19 @@ def get_item_from_json(input_json: str, input_key: str) -> str:
         return "Failed To Obtain Download Link From Json"
 
 
-def get_one_dir_up(input_dir: str) -> str:
-    up_dir = ""
-    return up_dir
+def get_one_dir_up(input_dir: pathlib.Path) -> pathlib.Path:
+    return pathlib.Path(input_dir).parents[0]
 
 
-CoDSpecific.download_info()
-time.sleep(999999)
-del_files_by_ext_in_tree(folder_to_process, ".tmp")
-CoDSpecific.download_mods(get_files_by_extension_in_tree(folder_to_process, ".json"))
-CoDSpecific.extract_mod_archives()
-CoDSpecific.organize_unzipped_mods()
+CoDSpecific.download_info(DIR_TO_PROCESS)
+del_files_by_ext_in_tree(DIR_TO_PROCESS, ".tmp")
+CoDSpecific.download_mods(get_files_by_extension_in_tree(DIR_TO_PROCESS, ".json"))
+CoDSpecific.extract_mod_archives(DIR_TO_PROCESS)
+CoDSpecific.organize_unzipped_mods(DIR_TO_PROCESS)
 CoDSpecific.print_final()
 time.sleep(999999)
-
 quit()
 
 # To Do List:
-# get one dir up function
 # finish organize_unzipped_mods
-# Replace os module usage with pathlib module usage
-# list incomplete entries and broken download links at end
-# "use type hints and pass around Path objects instead of strings" look into this
+# Replace os.walk with pathlib/r.glob
